2389ff11f94c8e5c8ef48aec1800199e7e6f6a79
ashwinrin77 <161035206+ashwinrin77@users.noreply.github.com>
2024-05-25 19:21:33 -1200

    Update tools.py

--- functions/tools.py
+++ functions/tools.py
@@ -239,7 +239,7 @@ class Tools:
             ss, dd = await self.duration_s(filename)
             __ = filename.split(".mkv")[-2]
             out = __ + "_sample.mkv"
-            _ncmd = f'ffmpeg -i """{filename}""" -preset ultrafast -ss {ss} -to {dd} -c:v libx265 -crf 27 -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? """{out}""" -y'
+            _ncmd = f'ffmpeg -i """{filename}""" -preset veryfast -ss {ss} -to {dd} -c:v libx265 -crf 27 -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? """{out}""" -y'
             process = await asyncio.create_subprocess_shell(
                 _ncmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
             )
